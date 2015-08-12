872cef0f938aeea305cf967104c8620903be7aca
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-08-12 19:02:24 -0300

    Bootstrap.

--- bustracker/apps/core/models.py
+++ bustracker/apps/core/models.py
@@ -11,6 +11,15 @@ class BusTerminal(models.Model):
     latitude = models.DecimalField(max_digits=8, decimal_places=5)
     longitude = models.DecimalField(max_digits=8, decimal_places=5)
 
+    def distance_from(self, latitude, longitude):
+        '''
+            This will return the distance in metters
+            between any point and this terminal.
+
+            Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula)
+        '''
+        return 0
+
 
 class BusRoute(models.Model):
     ''' Linha de ônibus. '''
@@ -28,3 +37,9 @@ class Bus(models.Model):
     slug = models.CharField(max_length=10)
     capacity = models.IntegerField(default=0)
     route = models.ForeignKey(BusRoute, related_name='bus_set')
+
+    def get_last_position(self):
+        ''' Return the last know bus position.
+        If there's no position return (0,0). '''
+        latitude = longitude = 0
+        return latitude, longitude
--- bustracker/apps/core/tests.py
+++ bustracker/apps/core/tests.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import BusTerminal
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_insert_bus_terminal(self):
+        BusTerminal.objects.create(name='TERMINAL PROEB',
+                                   slug='PROEB',
+                                   latitude=-26.8964962,
+                                   longitude=-49.1063695)
+
+        terminal = BusTerminal.objects.get(pk=1)
+        self.assertEquals(terminal.slug, 'PROEB')
