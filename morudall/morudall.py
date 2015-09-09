60ed382650cdcd45363871c9406fb0525a0ac0f3
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-09-09 01:12:42 -0300

    Refactor.

--- morudall/morudall.py
+++ morudall/morudall.py
@@ -1,36 +1,60 @@
+# -*- coding: utf-8 -*-
+
 import paho.mqtt.client as mqtt
 
-SERVER_IP = '45.55.201.214'
-SERVER_PORT = 1883
-SERVER_AUTH = ('guest', 'guest')
+from collections import namedtuple
 
 
-# The callback for when the client receives a CONNACK response from the server.
+Auth = namedtuple('Auth', ['user', 'pwd'])
+
+MQTT_ADDRESS = 'tcc.alexandrevicenzi.com'
+MQTT_PORT = 1883
+MQTT_AUTH = Auth('guest', 'guest')
+MQTT_TIMEOUT = 120
+
+MONGO_ADDRESS = MQTT_ADDRESS
+MONGO_PORT = 27017
+
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
-    client.subscribe("/location")
-    client.publish('/location', '@-26.8956032,-49.0794134')
+    print('Connected with result code %s' % str(rc))
+    client.subscribe('/location')
+    client.publish('/location', '@-26.8956032,-49.0794134,124123421,AAA1234')
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect(SERVER_IP, SERVER_PORT, 60)
+    if msg.topic == '/location' and msg.payload.startswith('@'):
+        lat, lon, ts, slug = msg.payload[1:].split(',')
+        client.save_data(client, slug, float(lat), float(lon), ts)
 
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+def save_data(client, slug, lat, lon, date):
+    client.db.add_location(slug, lat, lon, date)
 
 
-client.loop_forever()
+def log_data(client, slug, lat, lon, date):
+    print('latitude %f and longitude %f from %s at %s' % (lat, lon, slug, date))
+
+
+def loop(debug=False):
+    client = mqtt.Client()
+
+    if debug:
+        print('Debug mode enabled!')
+        client.save_data = log_data
+    else:
+        from garogh import Locator
+        db = Locator('mongodb://%s:%d/' % (MONGO_ADDRESS, MONGO_PORT))
+        client.db = db
+        client.save_data = save_data
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(MQTT_AUTH.user, MQTT_AUTH.pwd)
+    client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
+
+    client.loop_forever()
+
+if __name__ == '__main__':
+    import sys
+    loop('--debug' in sys.argv)
