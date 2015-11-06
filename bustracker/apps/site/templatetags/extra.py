26bd6dbfb4d4dfb4a94ac32a393cdaf785e91c27
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-11-06 02:18:06 -0200

    Mobile friendly.

--- bustracker/apps/site/templatetags/extra.py
+++ bustracker/apps/site/templatetags/extra.py
@@ -23,3 +23,14 @@ def do_ceil(value):
     if not value:
         return 0
     return math.ceil(value)
+
+
+@register.filter()
+def estimated_arrival_to(bus, station):
+    if not station:
+        return 0.0
+
+    try:
+        return bus.estimated_arrival_to(station.latitude, station.longitude).minutes
+    except:
+        return 0.0
--- bustracker/apps/site/views.py
+++ bustracker/apps/site/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from apps.core.models import BusStop, Bus
@@ -12,9 +13,26 @@ def about(request):
 
 
 def bus_stations(request):
-    l = list(BusStop.objects.filter(is_active=True, stop_type='bus-station'))
+    stop_id = request.GET.get('id')
+    station = None
+    bus_list = []
+
+    station_list = BusStop.objects.filter(is_active=True, stop_type='bus-station')
+
+    if stop_id:
+        stop_id = int(stop_id)
+
+        try:
+            station = BusStop.objects.get(pk=stop_id)
+        except BusStop.DoesNotExist:
+            pass
+
+        bus_list = Bus.objects.filter(Q(route__to_stop__pk=stop_id) | Q(route__stops__pk=stop_id), is_active=True)
+
     return render(request, 'site/bus_stations.html', {
-        'station_list': sorted([t.to_dict() for t in l], key=lambda t: t['name'])
+        'station_list': [s.to_dict() for s in station_list], #sorted([s.to_dict() for s in station_list], key=lambda t: s['name']),
+        'station': station,
+        'bus_list': bus_list,
     })
 
 
@@ -33,4 +51,8 @@ def bus_route(request):
             pass
 
     bus_list = Bus.objects.filter(is_active=True)
-    return render(request, 'site/bus_route.html', {'bus': bus, 'bus_list': bus_list})
+
+    return render(request, 'site/bus_route.html', {
+        'bus': bus,
+        'bus_list': bus_list,
+    })
